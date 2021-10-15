c5e15ac88046c64a6f70a0eec4d947572464bad1
Aaron Carvalho <carvalhosins@gmail.com>
2021-10-14 22:43:43 -0300

    Ajuste dos Elementos
    - Inserção de JavaScript
    - Refatoração do Css
    - Inserção de Animações
    - Mudança de Logo

---
+++ js/WebPageJs.js
@@ -0,0 +1,8 @@
+var control = document.getElementsByName("body");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+control.onload = function OnLoad()
+{    
+    modal.style.display = "block";
+}
